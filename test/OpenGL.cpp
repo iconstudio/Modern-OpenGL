e1cbbfcb71a4f76929fac53d6a2fbece1ac2b740
Iconer <yoyofa2@hotmail.com>
2023-06-16 07:31:12 +0900

    cmake 삭제

--- test/OpenGL.cpp
+++
@@ -1,9 +0,0 @@
-﻿// OpenGL.cpp : 애플리케이션의 진입점을 정의합니다.
-//
-#include "OpenGL.h"
-
-int main()
-{
-	std::cout << "Hello CMake." << std::endl;
-	return 0;
-}
--- test/OpenGL.h
+++
@@ -1,5 +0,0 @@
-﻿// OpenGL.h: 표준 시스템 포함 파일
-// 또는 프로젝트 특정 포함 파일이 들어 있는 포함 파일입니다.
-#pragma once
-#include <iostream>
-import Internal;
