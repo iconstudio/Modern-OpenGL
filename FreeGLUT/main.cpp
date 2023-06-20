27c10e4c143982ea57f43c5c6355b7bebfb65290
Iconer <yoyofa2@hotmail.com>
2023-06-21 02:57:07 +0900

    창 생성 테스트

--- FreeGLUT/main.cpp
+++ FreeGLUT/main.cpp
@@ -1,19 +1,39 @@
 #include "pch.hpp"
+#include "Internal.hpp"
 
 import Glib;
 import Glib.Device.ProcessInstance;
 import Glib.Device.Handle;
 import Glib.Device.Window;
 
+LRESULT CALLBACK WndProc(HWND hWnd, UINT Msg, WPARAM wParam, LPARAM lParam)
+{
+	return DefWindowProc(hWnd, Msg, wParam, lParam);
+}
+
+const auto& my_windows_class = L"MY_CLASS";
+
 int main(const int& argc, char** argv)
 {
 	gl::Initialize(gl::DisplayModes::DEPTH, gl::default_position, gl::default_resoulution);
 
 	auto hinstance = gl::device::GetProcessInstance();
-	//uto window = gl::device::ManagedHandle::Create(hinstance, L"class", L"title", 0U, -1, -1, 400, 300);
+	const int error0 = gl::device::GetLastError();
 
 
-	while (true)
+	auto property = gl::device::DeviceProperty{ hinstance, WndProc, my_windows_class };
+	const int error1 = gl::device::GetLastError();
+
+	property.Register();
+	const int error2 = gl::device::GetLastError();
+
+	auto window = gl::device::Window::Create(property, L"MY_TITLE", -1, -1, 400, 300);
+	const int error3 = gl::device::GetLastError();
+
+	window.Show();
+	const int error4 = gl::device::GetLastError();
+
+	//while (true)
 	{
 
 	}
