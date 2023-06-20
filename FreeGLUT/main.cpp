7fe0ed0f2d77e7b9ac9f78a9644f015b55f97998
Iconer <yoyofa2@hotmail.com>
2023-06-21 00:47:41 +0900

    실행 가능하게 수정

--- FreeGLUT/main.cpp
+++ FreeGLUT/main.cpp
@@ -3,13 +3,14 @@
 import Glib;
 import Glib.Device.ProcessInstance;
 import Glib.Device.Handle;
+import Glib.Device.Window;
 
 int main(const int& argc, char** argv)
 {
 	gl::Initialize(gl::DisplayModes::DEPTH, gl::default_position, gl::default_resoulution);
 
 	auto hinstance = gl::device::GetProcessInstance();
-	auto window = gl::device::ManagedHandle::Create(hinstance, L"class", L"title", 0U, -1, -1, 400, 300);
+	//uto window = gl::device::ManagedHandle::Create(hinstance, L"class", L"title", 0U, -1, -1, 400, 300);
 
 
 	while (true)
