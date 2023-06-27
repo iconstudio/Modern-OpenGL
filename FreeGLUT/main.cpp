e880c044201a07bffc461d4ee05eb2f254c6e98a
Iconer <yoyofa2@hotmail.com>
2023-06-27 15:05:31 +0900

    테스트 중

--- FreeGLUT/main.cpp
+++ FreeGLUT/main.cpp
@@ -11,48 +11,6 @@ import Glib.Device.Handle;
 import Glib.Window;
 import Glib.Window.ManagedWindow;
 
-LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam)
-{
-	switch (msg)
-	{
-		// Started by close button or system menu or Alt+F4
-		case WM_CLOSE:
-		{
-			::DestroyWindow(hwnd);
-		}
-		break;
-
-		// Started by DestroyWindow
-		case WM_DESTROY:
-		{
-			gl::window::PostQuitMessage(0);
-		}
-		break;
-
-		// Started by WM_DESTROY
-		case WM_QUIT:
-		{
-			//KillTimer(hwnd, RENDER_TIMER_ID);
-		}
-		break;
-
-		// Clean memory up
-		// Started by WM_DESTROY
-		case WM_NCDESTROY:
-		{
-			// Forced stop code
-			return 0;
-		}
-
-		default:
-		{
-			return ::DefWindowProc(hwnd, msg, wparam, lparam);
-		}
-	}
-
-	return 0;
-}
-
 const auto& my_windows_class = L"MY_CLASS";
 
 int main(const int& argc, char** argv)
@@ -60,13 +18,11 @@ int main(const int& argc, char** argv)
 	util::Println("Program Initiated");
 	gl::Initialize(gl::DisplayModes::DEPTH, gl::default_position, gl::default_resoulution);
 
-	auto hinstance = gl::device::GetProcessInstance();
-
-	gl::window::WindowProperty property = gl::window::CreateProperty(hinstance, WndProc, my_windows_class);
+	gl::window::WindowProperty property = gl::window::CreateProperty(gl::device::GetProcessInstance(), gl::window::ManagedWindow<"TestWindow">::MainWorker, my_windows_class);
 	property.Register();
 
 	util::Println("Creating a Window");
-	auto window = gl::window::CreateWindow(property, L"MY_TITLE", -1, -1, 400, 300);
+	auto window = gl::window::CreateWindow(property, L"MY_TITLE", 50, 70, 400, 300);
 
 	auto managed_window = gl::window::ManagedWindow<"TestWindow">(std::move(window));
 	managed_window.Awake();
