3a589ed80fcf3b5c522a80d0f43e0cbce0853ecd
Iconer <yoyofa2@hotmail.com>
2023-06-27 10:03:43 +0900

    소스 삭제

--- FreeGLUT/ManagedWindow.cpp
+++ FreeGLUT/ManagedWindow.cpp
@@ -3,13 +3,3 @@ module;
 
 module Glib.Window.ManagedWindow;
 import Utility;
-
-gl::window::ManagedWindow::ManagedWindow(Window&& window) noexcept
-	: underlying(std::move(window))
-	, optionFullscreen(false)
-	, myEventHandlers()
-	, std::enable_shared_from_this<ManagedWindow>()
-{
-	myDimensions = underlying.GetDimensions();
-	myEventHandlers.reserve(20);
-}
