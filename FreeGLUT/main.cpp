8f626a718655ce3ae8c1a18f0ac0ab6f283705b3
Iconer <yoyofa2@hotmail.com>
2023-06-27 12:42:53 +0900

    관리되는 창 객체에 이름을 붙이고 사용할 수 있음

--- FreeGLUT/main.cpp
+++ FreeGLUT/main.cpp
@@ -43,9 +43,14 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam)
 			// Forced stop code
 			return 0;
 		}
+
+		default:
+		{
+			return ::DefWindowProc(hwnd, msg, wparam, lparam);
+		}
 	}
 
-	return ::DefWindowProc(hwnd, msg, wparam, lparam);
+	return 0;
 }
 
 const auto& my_windows_class = L"MY_CLASS";
@@ -63,9 +68,15 @@ int main(const int& argc, char** argv)
 	util::Println("Creating a Window");
 	auto window = gl::window::CreateWindow(property, L"MY_TITLE", -1, -1, 400, 300);
 
-	gl::window::ManagedWindow managed_window = gl::window::ManagedWindow(std::move(window));
+	auto managed_window = gl::window::ManagedWindow<"TestWindow">(std::move(window));
 	managed_window.Awake();
 	managed_window.Start();
+
+	//gl::window::ManagedWindow<"TestWindow">::Instance->Awake();
+	//gl::window::ManagedWindow<"TestWindow">::Instance->Start();
+
+	//util::Println("handle is {}.", reinterpret_cast<void*>(gl::window::ManagedWindow<"TestWindow">::Instance));
+	//util::Println("{}.", gl::window::ManagedWindow<"TestWindow">::Instance->DefaultEventID);
 
 	util::CancellationSource cancellation_source{};
 	util::Println("Program Started");
