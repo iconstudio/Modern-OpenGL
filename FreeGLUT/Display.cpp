cc5119632d1c62aaa6fea3b2f04958c050f640f6
Iconer <yoyofa2@hotmail.com>
2023-06-30 06:24:02 +0900

    임시 코드 주석화

--- FreeGLUT/Display.cpp
+++ FreeGLUT/Display.cpp
@@ -11,15 +11,15 @@ namespace gl::display
 	bool IsDimmingMode()
 	{
 		UISettings settings = UISettings();
-		const device::Colour col_a_0 = settings.GetColorValue(UIColorType::Accent);
-		const device::Colour col_a_1 = settings.GetColorValue(UIColorType::AccentDark1);
-		const device::Colour col_a_2 = settings.GetColorValue(UIColorType::AccentDark2);
-		const device::Colour col_a_3 = settings.GetColorValue(UIColorType::AccentDark3);
-		const device::Colour col_a_4 = settings.GetColorValue(UIColorType::AccentLight1);
-		const device::Colour col_a_5 = settings.GetColorValue(UIColorType::AccentLight2);
-		const device::Colour col_a_6 = settings.GetColorValue(UIColorType::AccentLight3);
+		//const device::Colour col_a_0 = settings.GetColorValue(UIColorType::Accent);
+		//const device::Colour col_a_1 = settings.GetColorValue(UIColorType::AccentDark1);
+		//const device::Colour col_a_2 = settings.GetColorValue(UIColorType::AccentDark2);
+		//const device::Colour col_a_3 = settings.GetColorValue(UIColorType::AccentDark3);
+		//const device::Colour col_a_4 = settings.GetColorValue(UIColorType::AccentLight1);
+		//const device::Colour col_a_5 = settings.GetColorValue(UIColorType::AccentLight2);
+		//const device::Colour col_a_6 = settings.GetColorValue(UIColorType::AccentLight3);
 		const device::Colour col_fg = settings.GetColorValue(UIColorType::Foreground);
-		const device::Colour col_bk = settings.GetColorValue(UIColorType::Background);
+		//const device::Colour col_bk = settings.GetColorValue(UIColorType::Background);
 
 		if (device::IsColorBright(col_fg))
 		{
