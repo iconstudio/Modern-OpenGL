b69c3c6cf839a5469587eab003c218f17f96a070
Iconer <yoyofa2@hotmail.com>
2023-07-01 05:09:43 +0900

    안 쓰는 소스 파일들 삭제

--- FreeGLUT/DeviceColour.cpp
+++
@@ -1,1 +0,0 @@
-module Glib.Device.Colour;
--- FreeGLUT/ManagedWindow.cpp
+++
@@ -1,9 +0,0 @@
-module;
-#include "Internal.hpp"
-#undef LOWORD
-#undef HIWORD
-#undef CreateWindow
-#undef CreateWindowEx
-
-module Glib.Window.ManagedWindow;
-import Utility.FixedString;
