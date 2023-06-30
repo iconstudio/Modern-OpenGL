d5fa4c864eac432ebc1c9d7edcb9747f29318f8f
Iconer <yoyofa2@hotmail.com>
2023-06-30 15:32:48 +0900

    안 쓰는 모듈 가져오기 삭제

--- FreeGLUT/main.cpp
+++ FreeGLUT/main.cpp
@@ -2,9 +2,6 @@
 import Utility.FixedString;
 import Utility.Print;
 import Glib;
-import Glib.Device.ProcessInstance;
-import Glib.Device.Handle;
-import Glib.Window;
 import Glib.Window.ManagedWindow;
 
 static inline constexpr util::basic_fixed_string my_windows_class = L"MY_CLASS";
@@ -15,11 +12,11 @@ int main([[maybe_unused]] const int& argc, [[maybe_unused]] const char** const& 
 
 	gl::Initialize(gl::DisplayModes::DEPTH, gl::default_position, gl::default_resoulution);
 
-	gl::window::WindowProperty property = gl::window::CreatePropertyEx<my_windows_class>();
+	auto property = gl::window::CreatePropertyEx<my_windows_class>();
 	property.Register();
 
 	util::Println("== Creating the Client ==");
-	auto window = gl::window::CreateWindowEx<my_windows_class>(property, L"MY_TITLE", 50, 70, 400, 300);
+	auto window = gl::window::CreateWindowEx<my_windows_class>(property, L"MY_TITLE", 50, 70, 640, 480);
 
 	window.Awake();
 
