864460b64d5830766eac8630b7eee629b608b89f
Iconer <yoyofa2@hotmail.com>
2023-07-02 12:34:47 +0900

    속성 없이 `CreateWindowEx`에서 창을 바로 만듦

--- FreeGLUT/main.cpp
+++ FreeGLUT/main.cpp
@@ -17,20 +17,17 @@ int main([[maybe_unused]] const int& argc, [[maybe_unused]] const char** const& 
 
 	gl::Initialize(gl::DisplayModes::DEPTH, gl::default_position, gl::default_resoulution);
 
-	auto property = gl::window::CreateProperty<my_windows_class>(gl::window::ManagedWindow::MainWorker);
-	property.Register();
-
 	util::Println("== Creating the Client ==");
-	auto window = gl::window::CreateWindowEx<my_windows_class>(property, L"MY_TITLE", 50, 70, 640, 480);
+	auto window = gl::window::CreateWindowEx<my_windows_class>(L"MY_TITLE", 50, 70, 640, 480);
 
-	window.Awake();
+	window->Awake();
 
 	util::Println("== Program Started ==");
 
 	gl::Start();
 
 	//window.StartCoroutine(test_coroutine());
-	window.Start();
+	window->Start();
 
 	util::Println("== Program Ended ==");
 
