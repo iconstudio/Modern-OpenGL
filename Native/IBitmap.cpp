da0b05b39b69bbbd61c321f04a167bb92725046c
Iconer <yoyofa2@hotmail.com>
2023-07-03 23:44:23 +0900

    비트맵 인터페이스 모듈의 소스 파일 추가

---
+++ Native/IBitmap.cpp
@@ -0,0 +1,16 @@
+module;
+#include "Internal.hpp"
+module Glib.Device.Resource.IBitmap;
+
+using namespace gl::device::resource;
+
+IBitmap IBitmap::Copy() const noexcept
+{
+	auto& handle = GetHandle();
+	if (handle)
+	{
+	}
+
+
+	return IBitmap();
+}
