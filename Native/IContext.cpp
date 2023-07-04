4e9e73417d7e324ae05b466e3cbe4fb8038ccc73
Iconer <yoyofa2@hotmail.com>
2023-07-05 07:59:40 +0900

    문맥 인터페이스를 제거하는 메서드 구현

--- Native/IContext.cpp
+++ Native/IContext.cpp
@@ -6,19 +6,19 @@ bool
 gl::device::IContext::Destroy()
 noexcept
 {
-	return false;
+	return 0 != Delegate(::DeleteDC);
 }
 
 bool
 gl::device::IContext::Release()
 noexcept
 {
-	return false;
+	return 0 != ::ReleaseDC(nullptr, GetHandle());
 }
 
 bool
 gl::device::IContext::Release(gl::device::native::HWND& owner)
 noexcept
 {
-	return false;
+	return 0 != ::ReleaseDC(owner, GetHandle());
 }
