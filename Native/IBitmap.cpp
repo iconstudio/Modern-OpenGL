e1e76386e86ef5835b088b616a50a2f9bb9a5b98
Iconer <yoyofa2@hotmail.com>
2023-07-04 17:06:13 +0900

    비트맵 인터페이스의 생성자 Getter 메서드 구현

--- Native/IBitmap.cpp
+++ Native/IBitmap.cpp
@@ -2,15 +2,150 @@ module;
 #include "Internal.hpp"
 module Glib.Device.Resource.IBitmap;
 
-using namespace gl::device::resource;
-
-IBitmap IBitmap::Copy() const noexcept
+gl::device::resource::IBitmap
+gl::device::resource::IBitmap::Copy()
+const noexcept
 {
 	auto& handle = GetHandle();
 	if (handle)
 	{
 	}
 
-
 	return IBitmap();
+}
+
+bool
+gl::device::resource::IBitmap::TryCopy(IBitmap& output)
+const noexcept
+{
+	auto& handle = GetHandle();
+	if (nullptr == handle)
+	{
+		return false;
+	}
+
+	if (0 == cachedWidth || 0 == cachedHeight)
+	{
+		return false;
+	}
+
+	return false;
+}
+
+bool
+gl::device::resource::IBitmap::Destroy()
+noexcept
+{
+	if (nullptr != GetHandle())
+	{
+		bool result = 0 != Delegate(::DeleteObject);
+		base::operator=(nullifier);
+
+		return result;
+	}
+
+	return false;
+}
+
+void
+gl::device::resource::IBitmap::Mirror()
+noexcept
+{}
+
+void
+gl::device::resource::IBitmap::Flip()
+noexcept
+{}
+
+void
+gl::device::resource::IBitmap::Rotate(float angle)
+noexcept
+{}
+
+void
+gl::device::resource::IBitmap::RotateR(float angle)
+noexcept
+{}
+
+void
+gl::device::resource::IBitmap::RotateL(float angle)
+noexcept
+{}
+
+bool
+gl::device::resource::IBitmap::Fill()
+noexcept
+{
+	return false;
+}
+
+bool
+gl::device::resource::IBitmap::Draw(const ISurface& context, const int& x, const int& y)
+const noexcept
+{
+	return false;
+}
+
+bool
+gl::device::resource::IBitmap::GetPixel(const ISurface& context, const int& x, const int& y, Colour& output)
+const
+{
+	if (RawRGB result = context.Delegate(::GetPixel, x, y); CLR_INVALID != result)
+	{
+		output = device::MakeColor(result);
+
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+
+int
+gl::device::resource::IBitmap::GetWidth()
+const noexcept
+{
+	return cachedWidth;
+}
+
+int
+gl::device::resource::IBitmap::GetHeight()
+const noexcept
+{
+	return cachedHeight;
+}
+
+gl::device::resource::IBitmap::IBitmap(const handle_type& handle)
+noexcept
+	: base(handle)
+{
+	::SIZE result{};
+	if (0 != ::GetBitmapDimensionEx(handle, &result))
+	{
+		cachedWidth = result.cx;
+		cachedHeight = result.cy;
+	}
+}
+
+gl::device::resource::IBitmap::IBitmap(handle_type&& handle)
+noexcept
+	: base(std::move(handle))
+	, shouldDestroy(true)
+{
+	::SIZE result{};
+	if (0 != ::GetBitmapDimensionEx(GetHandle(), &result))
+	{
+		cachedWidth = result.cx;
+		cachedHeight = result.cy;
+	}
+}
+
+gl::device::resource::IBitmap::~IBitmap()
+noexcept
+{
+	if (shouldDestroy)
+	{
+		Destroy();
+	}
 }
--- Native/IBitmap.ixx
+++ Native/IBitmap.ixx
@@ -4,7 +4,9 @@ module;
 export module Glib.Device.Resource.IBitmap;
 import <type_traits>;
 import Glib.Device.Definitions;
-import Glib.Device.IHandle;
+import Glib.Device.IGraphics;
+import Glib.Device.ISurface;
+import Glib.Device.Colour;
 
 export namespace gl::device::resource
 {
@@ -13,15 +15,21 @@ export namespace gl::device::resource
 	public:
 	};
 
-	class [[nodiscard]] IBitmap : public IHandle<native::RawBitmap>
+	/// <summary>
+	/// Bitmap Viewer
+	/// </summary>
+	class [[nodiscard]] IBitmap : public IGraphics<IBitmap, native::RawBitmap>
 	{
 	public:
-		using base = IHandle<native::RawBitmap>;
+		using base = IGraphics<IBitmap, native::RawBitmap>;
 
 		constexpr IBitmap() noexcept = default;
+		explicit IBitmap(const handle_type& handle) noexcept;
+		explicit IBitmap(handle_type&& handle) noexcept;
+		~IBitmap() noexcept;
 
 		constexpr IBitmap(nullptr_t) noexcept
-			: base(nullptr)
+			: base(nullifier)
 		{}
 
 		[[nodiscard]] IBitmap Copy() const noexcept;
@@ -38,10 +46,19 @@ export namespace gl::device::resource
 
 		bool Fill() noexcept;
 
-		bool Draw(const native::NativeSurface& hdc, const int& x, const int& y) const noexcept;
+		bool Draw(const ISurface& context, const int& x, const int& y) const noexcept;
 
-		bool GetPixel(const int& x, const int& y, int& output) const noexcept;
+		bool GetPixel(const ISurface& context, const int& x, const int& y, Colour& output) const;
 		int GetWidth() const noexcept;
 		int GetHeight() const noexcept;
+
+		IBitmap(const IBitmap&) = delete;
+		constexpr IBitmap(IBitmap&&) noexcept = default;
+		IBitmap& operator=(const IBitmap&) = delete;
+		constexpr IBitmap& operator=(IBitmap&&) noexcept = default;
+
+	private:
+		bool shouldDestroy = false;
+		int cachedWidth = 0, cachedHeight = 0;
 	};
 }
