70f07956e3745002993011e2c3e517a6c269b174
Iconer <yoyofa2@hotmail.com>
2023-07-05 10:03:36 +0900

    창 핸들로부터 장치 문맥 객체를 생성할 수 있음

--- Native/DeviceContext.cpp
+++ Native/DeviceContext.cpp
@@ -2,6 +2,12 @@ module;
 #include "Internal.hpp"
 module Glib.Device.Context;
 
+gl::device::DeviceContext::DeviceContext(const native::HWND& window)
+noexcept
+	: myWindowHandle(window)
+	, base(::GetDC(window))
+{}
+
 gl::device::DeviceContext::~DeviceContext()
 noexcept
 {
