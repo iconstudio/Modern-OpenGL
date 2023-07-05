99903f8e8c09bc0e3eb63e8e87af7a107e4d35d2
Iconer <yoyofa2@hotmail.com>
2023-07-05 21:16:45 +0900

    외부 이미지 불러오기 함수 구현

--- Native/ImageLoader.cpp
+++ Native/ImageLoader.cpp
@@ -4,17 +4,49 @@ import Utility.Monad.Loosen;
 
 using gl::device::FilePath;
 
-inline constexpr unsigned int DefaultFlag = LR_LOADFROMFILE | LR_DEFAULTSIZE;
+inline constexpr unsigned int _DefaultFlag = LR_LOADFROMFILE | LR_DEFAULTSIZE;
 
 util::LooseMonad<HBITMAP, unsigned int>
 _LoadBitmap(const FilePath& path)
 noexcept
 {
-	HANDLE handle = LoadImage(nullptr, path.c_str(), IMAGE_BITMAP, 0, 0, DefaultFlag);
+	HANDLE handle = ::LoadImage(nullptr, path.c_str(), IMAGE_BITMAP, 0, 0, _DefaultFlag);
 	if (nullptr == handle)
 	{
 		return ::GetLastError();
 	}
+	else
+	{
+		return static_cast<HBITMAP>(handle);
+	}
+}
 
-	return static_cast<HBITMAP>(handle);
+util::LooseMonad<HICON, unsigned int>
+_LoadIcon(const FilePath& path)
+noexcept
+{
+	HANDLE handle = ::LoadImage(nullptr, path.c_str(), IMAGE_ICON, 0, 0, _DefaultFlag);
+	if (nullptr == handle)
+	{
+		return ::GetLastError();
+	}
+	else
+	{
+		return static_cast<HICON>(handle);
+	}
+}
+
+util::LooseMonad<HCURSOR, unsigned int>
+_LoadCursor(const FilePath& path)
+noexcept
+{
+	HANDLE handle = ::LoadImage(nullptr, path.c_str(), IMAGE_CURSOR, 0, 0, _DefaultFlag);
+	if (nullptr == handle)
+	{
+		return ::GetLastError();
+	}
+	else
+	{
+		return static_cast<HCURSOR>(handle);
+	}
 }
