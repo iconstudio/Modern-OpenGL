e0d3cc080185a5b1996fae0d37458738d1693bc6
Iconer <yoyofa2@hotmail.com>
2023-07-07 18:34:27 +0900

    이벤트 API 구현 파일의 문법 오류 수정

--- Native/src/EventAPI.cpp
+++ Native/src/EventAPI.cpp
@@ -1,3 +1,18 @@
 module;
 #include "Internal.hpp"
-export module Glib.Device.Event.API;
+module Glib.Device.Event.API;
+
+bool
+gl::device::EventAPI::Pop(const native::HWND& hwnd, gl::device::RawEvent& output)
+noexcept
+{
+	return 0 != ::GetMessage(&output, hwnd, 0, 0);
+}
+
+bool
+gl::device::EventAPI::Push(const native::HWND& hwnd, const unsigned int& id, const unsigned long long& lhs, const long long& rhs)
+noexcept
+
+{
+	return 0 != ::PostMessage(hwnd, id, lhs, rhs);
+}
