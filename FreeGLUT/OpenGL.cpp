77d606382f86962a7900c7fe88383be54f076114
Iconer <yoyofa2@hotmail.com>
2023-07-08 23:36:16 +0900

    기존에 정의했던 프레임워크 초기화 함수들 삭제

--- FreeGLUT/OpenGL.cpp
+++ FreeGLUT/OpenGL.cpp
@@ -5,14 +5,12 @@ import Utility.Print;
 import Utility.FixedString;
 import Glib.Rect;
 import Glib.Device.Colour;
-import Glib.Display;
 
 static inline int window_handle = -1;
 static inline gl::DisplayModes window_mode{};
-static inline gl::Rect window_rect{};
 
 std::shared_ptr<gl::Framework>
-gl::CreateFramework()
+gl::InitializeFramework()
 noexcept
 {
 	return std::shared_ptr<Framework>();
@@ -27,26 +25,6 @@ void gl::Initialize(const DisplayModes& mode, const int& x, const int& y, const 
 	window_rect.w = w;
 	window_rect.h = h;
 	window_mode = mode;
-}
-
-void gl::Initialize(const DisplayModes& mode, default_position_t, const int& w, const int& h) noexcept
-{
-	return Initialize(mode, 400, 400, w, h);
-}
-
-void gl::Initialize(const DisplayModes& mode, const int& x, const int& y, default_resoulution_t) noexcept
-{
-	return Initialize(mode, x, y, default_window_w, default_window_h);
-}
-
-void gl::Initialize(const DisplayModes& mode, const int& w, const int& h) noexcept
-{
-	return Initialize(mode, 400, 400, w, h);
-}
-
-void gl::Initialize(const DisplayModes& mode, default_position_t, default_resoulution_t) noexcept
-{
-	return Initialize(mode, 400, 400, default_window_w, default_window_h);
 }
 
 void gl::Start()
--- FreeGLUT/OpenGL.ixx
+++ FreeGLUT/OpenGL.ixx
@@ -12,13 +12,5 @@ export namespace gl
 	constexpr int default_window_w = 800;
 	constexpr int default_window_h = 600;
 
-	std::shared_ptr<Framework> CreateFramework() noexcept;
-
-	void Initialize(const DisplayModes& mode, const int& x, const int& y, const int& w, const int& h) noexcept;
-	void Initialize(const DisplayModes& mode, default_position_t, const int& w, const int& h) noexcept;
-	void Initialize(const DisplayModes& mode, const int& x, const int& y, default_resoulution_t) noexcept;
-	void Initialize(const DisplayModes& mode, const int& w, const int& h) noexcept;
-	void Initialize(const DisplayModes& mode, default_position_t, default_resoulution_t) noexcept;
-
-	void Start();
+	std::shared_ptr<Framework> InitializeFramework() noexcept;
 }
