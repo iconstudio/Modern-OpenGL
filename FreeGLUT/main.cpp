1a313a4609da95a84a332f93983d8620ac857841
Iconer <yoyofa2@hotmail.com>
2023-07-09 20:40:36 +0900

    필요없는 창 속성 삭제

--- FreeGLUT/main.cpp
+++ FreeGLUT/main.cpp
@@ -1,9 +1,6 @@
 #include "pch.hpp"
-import Utility.FixedString;
 import Utility.Print;
 import Glib;
-
-static inline constexpr util::basic_fixed_string my_windows_class = L"MY_CLASS";
 
 int main([[maybe_unused]] const int& argc, [[maybe_unused]] const char** const& argv)
 {
