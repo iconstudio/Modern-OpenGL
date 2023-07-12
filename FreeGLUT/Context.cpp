2b546de46b8c56615cf9c48cc71a4f358432f793
Iconer <yoyofa2@hotmail.com>
2023-07-12 16:28:14 +0900

    OpenGL 문맥 클래스에서 OpenGL 초기화를 수행함

--- FreeGLUT/Context.cpp
+++ FreeGLUT/Context.cpp
@@ -45,23 +45,43 @@ noexcept
 {
 	auto& handle = GetHandle();
 
+	const int target = ChoosePixelFormat(hdc, &opengl_format);
+	if (0 == target)
+	{
+		unsigned long error = ::GetLastError();
+		std::printf("Failed on acquiring a pixel format. (code: %u)\n", error);
+		return error;
+	}
+
+	PIXELFORMATDESCRIPTOR checker{};
+
+	const int check = DescribePixelFormat(hdc, target, sizeof(checker), &checker);
+	if (0 == check)
+	{
+		unsigned long error = ::GetLastError();
+		std::printf("Failed on reading a pixel format %d. (code: %u)\n", target, error);
+		return error;
+	}
+
+	if (0 == (checker.dwFlags & PFD_SUPPORT_OPENGL))
+	{
+		unsigned long error = ::GetLastError();
+		std::printf("Failed on checking a pixel format %d. (code: %u)\n", target, error);
+		return error;
+	}
+
+	if (0 == SetPixelFormat(hdc, target, &checker))
+	{
+		unsigned long error = ::GetLastError();
+		std::printf("Failed on setting pixel format %d. (code: %u)\n", target, error);
+		return error;
+	}
+
 	handle = ::wglCreateContext(hdc);
 	if (nullptr == handle)
 	{
-		const int current_format_id = GetPixelFormat(hdc);
 		unsigned long error = ::GetLastError();
-
-		if (0 == DescribePixelFormat(hdc, current_format_id, sizeof(pixel_format), &pixel_format))
-		{
-			std::printf("Failed on reading a pixel format %d. (code: %u)\n", current_format_id, error);
-		}
-
-		if (0 == (pixel_format.dwFlags & PFD_SUPPORT_OPENGL))
-		{
-			unsigned long error = ::GetLastError();
-			std::printf("Failed on checking a pixel format %d. (code: %u)\n", current_format_id, error);
-		}
-
+		std::printf("Failed on creating a opengl context. (code: %u)\n", error);
 		return error;
 	}
 
