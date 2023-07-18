9342ea97402be7a7d78e42ca59854ad38495b98b
Iconer <yoyofa2@hotmail.com>
2023-07-19 02:24:11 +0900

    은면소거 설정 메서드 구현

--- OpenGL/src/Culling.cpp
+++ OpenGL/src/Culling.cpp
@@ -2,3 +2,17 @@ module;
 #include <Windows.h>
 #include <gl\gl.h>
 module Glib.Culling;
+
+void
+gl::Culling(Face face)
+noexcept
+{
+	::glCullFace(static_cast<GLenum>(face));
+}
+
+void
+gl::CullingDirection(bool clockwise)
+noexcept
+{
+	::glFrontFace(clockwise ? GL_CW : GL_CCW);
+}
