a123cd537475de1dee2e58ab9ce42ed0da363e58
Iconer <yoyofa2@hotmail.com>
2023-07-23 19:47:57 +0900

    테스트 전용 프로젝트 추가

---
+++ TestTriangle/TestTriangle.cpp
@@ -0,0 +1,25 @@
+﻿import Utility.Print;
+import Glib.Framework;
+
+int main()
+{
+	util::Println("== Creating the Program ==");
+	auto framework = gl::CreateFramework();
+
+	util::Println("== Program Initiated ==");
+	gl::framework::Descriptor descriptor = gl::framework::MakeDefaultDescriptor();
+	descriptor.glDescriptor.alphaBlend = true;
+	descriptor.glDescriptor.doubleBuffered = true;
+	descriptor.glDescriptor.keepAspectRatio = true;
+	descriptor.glDescriptor.vSync = true;
+	descriptor.isResizable = false;
+
+	framework->Initialize(std::move(descriptor));
+
+	util::Println("== Program Started ==");
+	framework->Run();
+
+	util::Println("== Program Ended ==");
+
+	return 0;
+}
