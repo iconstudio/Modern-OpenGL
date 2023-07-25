a96439dbe8947927f71787bb936e1b22568d211b
Iconer <yoyofa2@hotmail.com>
2023-07-26 04:26:25 +0900

    변환 함수 추가

--- OpenGL/Transform.ixx
+++ OpenGL/Transform.ixx
@@ -6,6 +6,19 @@ export namespace gl
 	namespace transform
 	{
 		void LoadIdentity() noexcept;
+		void LookAt(float x, float y, float z, float tx, float ty, float tz, float ux, float uy, float uz) noexcept;
+		void LookAt(double x, double y, double z, double tx, double ty, double tz, double ux, double uy, double uz) noexcept;
+		void Projection(float fov, float aspect, float near, float far) noexcept;
+		void Projection(double fov, double aspect, double near, double far) noexcept;
+		void Ortho(float left, float right, float bottom, float top, float near, float far) noexcept;
+		void Ortho(double left, double right, double bottom, double top, double near, double far) noexcept;
+
+		void Translate(float x, float y, float z) noexcept;
+		void Translate(double x, double y, double z) noexcept;
+		void Rotate(float angle, float x, float y, float z) noexcept;
+		void Rotate(double angle, double x, double y, double z) noexcept;
+		void Scale(float x, float y, float z) noexcept;
+		void Scale(double x, double y, double z) noexcept;
 
 		void GetCurrentMatrix(TransformMode state, float(&matrix)[16]) noexcept;
 		void GetCurrentMatrix(TransformMode state, float* const& matrix) noexcept;
--- OpenGL/src/Transform.cpp
+++ OpenGL/src/Transform.cpp
@@ -1,6 +1,9 @@
 module;
 #include <Windows.h>
 #include <gl\gl.h>
+#include <gl\glu.h>
+#undef near
+#undef far
 
 module Glib;
 import :Transform;
@@ -10,10 +13,90 @@ gl::transform::LoadIdentity()
 noexcept
 {
 	::glLoadIdentity();
+}
 
-	::glFrustum;
-	::glOrtho;
+void
+gl::transform::LookAt(float x, float y, float z, float tx, float ty, float tz, float ux, float uy, float uz)
+noexcept
+{
+	::gluLookAt(double(x), double(y), double(z), double(tx), double(ty), double(tz), double(ux), double(uy), double(uz));
+}
 
+void
+gl::transform::LookAt(double x, double y, double z, double tx, double ty, double tz, double ux, double uy, double uz)
+noexcept
+{
+	::gluLookAt(x, y, z, tx, ty, tz, ux, uy, uz);
+}
+
+void
+gl::transform::Projection(float fov, float aspect, float near, float far)
+noexcept
+{
+	::gluPerspective(double(fov), double(aspect), double(near), double(far));
+}
+
+void
+gl::transform::Projection(double fov, double aspect, double near, double far)
+noexcept
+{
+	::gluPerspective(fov, aspect, near, far);
+}
+
+void
+gl::transform::Ortho(float left, float right, float bottom, float top, float near, float far)
+noexcept
+{
+	::glOrtho(double(left), double(right), double(bottom), double(top), double(near), double(far));
+}
+
+void
+gl::transform::Ortho(double left, double right, double bottom, double top, double near, double far)
+noexcept
+{
+	::glOrtho(left, right, bottom, top, near, far);
+}
+
+void
+gl::transform::Translate(float x, float y, float z)
+noexcept
+{
+	::glTranslatef(x, y, z);
+}
+
+void
+gl::transform::Translate(double x, double y, double z)
+noexcept
+{
+	::glTranslated(x, y, z);
+}
+
+void
+gl::transform::Rotate(float angle, float x, float y, float z)
+noexcept
+{
+	::glRotatef(angle, x, y, z);
+}
+
+void
+gl::transform::Rotate(double angle, double x, double y, double z)
+noexcept
+{
+	::glRotated(angle, x, y, z);
+}
+
+void
+gl::transform::Scale(float x, float y, float z)
+noexcept
+{
+	::glScalef(x, y, z);
+}
+
+void
+gl::transform::Scale(double x, double y, double z)
+noexcept
+{
+	::glScaled(x, y, z);
 }
 
 void
