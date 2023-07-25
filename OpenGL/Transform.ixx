64d23004c679836cff661aa55714be75cf5422be
Iconer <yoyofa2@hotmail.com>
2023-07-25 13:03:32 +0900

    현재 변환 행렬을 반환하는 함수 추가

--- OpenGL/Transform.ixx
+++ OpenGL/Transform.ixx
@@ -6,6 +6,10 @@ export namespace gl
 	namespace transform
 	{
 		void LoadIdentity() noexcept;
-		void GetMatrix(TransformMode mode, float* matrix) noexcept;
+
+		void GetCurrentMatrix(TransformMode state, float(&matrix)[16]) noexcept;
+		void GetCurrentMatrix(TransformMode state, float* const& matrix) noexcept;
+		void GetCurrentMatrix(TransformMode state, double(&matrix)[16]) noexcept;
+		void GetCurrentMatrix(TransformMode state, double* const& matrix) noexcept;
 	}
 }
--- OpenGL/src/Transform.cpp
+++ OpenGL/src/Transform.cpp
@@ -10,4 +10,36 @@ gl::transform::LoadIdentity()
 noexcept
 {
 	::glLoadIdentity();
+
+	::glFrustum;
+	::glOrtho;
+
+}
+
+void
+gl::transform::GetCurrentMatrix(TransformMode state, float(&matrix)[16])
+noexcept
+{
+	::glGetFloatv(static_cast<GLenum>(state), matrix);
+}
+
+void
+gl::transform::GetCurrentMatrix(TransformMode state, float* const& matrix)
+noexcept
+{
+	::glGetFloatv(static_cast<GLenum>(state), matrix);
+}
+
+void
+gl::transform::GetCurrentMatrix(TransformMode state, double(&matrix)[16])
+noexcept
+{
+	::glGetDoublev(static_cast<GLenum>(state), matrix);
+}
+
+void
+gl::transform::GetCurrentMatrix(TransformMode state, double* const& matrix)
+noexcept
+{
+	::glGetDoublev(static_cast<GLenum>(state), matrix);
 }
