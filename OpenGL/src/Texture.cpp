41a85f6e0379deae41e14b21ee17d39765da1f32
Iconer <yoyofa2@hotmail.com>
2023-07-29 10:06:28 +0900

    텍스쳐 생성 함수 몸체만 구현

--- OpenGL/src/Texture.cpp
+++ OpenGL/src/Texture.cpp
@@ -7,3 +7,30 @@ gl::Texture::Texture(nullptr_t)
 noexcept
 	: base()
 {}
+
+gl::Texture
+gl::Texture::EmptyTexture(std::uint32_t w, std::uint32_t h)
+noexcept
+{
+	return Texture();
+}
+
+gl::Texture
+gl::CreateEmptyTexture(std::uint32_t w, std::uint32_t h)
+noexcept
+{
+	return Texture();
+}
+
+gl::Texture
+gl::LoadTexture(const gl::FilePath& path)
+{
+	return Texture();
+}
+
+bool
+gl::TryLoadTexture(const gl::FilePath& path, gl::Texture& output)
+noexcept
+{
+	return false;
+}
