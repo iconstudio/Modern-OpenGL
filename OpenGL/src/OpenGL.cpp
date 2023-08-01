c9f322a86eac186d3f22da70a2eaeb4a0c8ed0c4
Iconer <yoyofa2@hotmail.com>
2023-08-02 00:01:34 +0900

    glew 초기화

--- OpenGL/src/OpenGL.cpp
+++ OpenGL/src/OpenGL.cpp
@@ -3,3 +3,28 @@ module;
 #include <GL/glew.h>
 #include <GL/GL.h>
 module Glib;
+import <stdexcept>;
+import <cstdio>;
+import Utility.Print;
+
+constinit static const GLubyte* error_string = nullptr;
+
+namespace gl
+{
+	void Initialize()
+	{
+		GLenum err = glewInit();
+		if (GLEW_OK != err)
+		{
+			error_string = glewGetErrorString(err);
+			const char* temp_msg = reinterpret_cast<const char*>(error_string);
+
+			std::printf("Error: %s\n", temp_msg);
+			//util::Println("Error: {}", temp_msg));
+			//util::Println(std::format("Error: {}", temp_msg));
+			//util::Println(std::vformat("Error: {}", std::make_format_args(temp_msg)));
+
+			throw std::runtime_error("Failed to initialize GLEW");
+		}
+	}
+}
