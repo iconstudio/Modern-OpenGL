ba4f4d865b1a9ec89f26e5553c6880c3506732f6
Iconer <yoyofa2@hotmail.com>
2023-08-07 19:55:14 +0900

    안 쓰는 클라이언트 실행부 파일 삭제

--- OpenGL/Framework.ixx
+++
@@ -1,5 +0,0 @@
-export module Glib.Framework;
-
-export namespace gl
-{
-}
--- OpenGL/main.cpp
+++
@@ -1,6 +0,0 @@
-import Utility.Print;
-import Glib;
-
-int main([[maybe_unused]] const int& argc, [[maybe_unused]] const char** const& argv)
-{
-}
