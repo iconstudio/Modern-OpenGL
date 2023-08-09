d8f4b8a2a1c996fd12449344a87262fcd913598c
Iconer <yoyofa2@hotmail.com>
2023-08-09 21:21:56 +0900

    공유 버퍼 객체 클래스 구현

--- OpenGL/src/SharedBufferObject.cpp
+++ OpenGL/src/SharedBufferObject.cpp
@@ -1,3 +1,11 @@
 module;
 module Glib;
+import <memory>;
+import :BufferObject;
 import :SharedBufferObject;
+
+void
+gl::SharedBufferObject::Create(gl::buffer::BufferUsage usage, const size_t& bytes_size)
+{
+	myBlob = std::make_shared<float[]>(bytes_size);
+}
