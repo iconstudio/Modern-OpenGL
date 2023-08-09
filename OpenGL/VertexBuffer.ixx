4e5ac9f3d33067a71cd3f014a8be02907260df8c
Iconer <yoyofa2@hotmail.com>
2023-08-09 20:54:17 +0900

    정점 버퍼는 수정 가능한 레이아웃을 제공하는 구현체를 상속함

--- OpenGL/VertexBuffer.ixx
+++ OpenGL/VertexBuffer.ixx
@@ -5,36 +5,22 @@ import :BufferObject;
 
 export namespace gl
 {
-	class [[nodiscard]]
-		alignas(std::hardware_constructive_interference_size)
-		VertexBuffer
+	class [[nodiscard]] VertexBuffer
+		: public gl::detail::BufferInterface<true>
 	{
 	public:
+		using base = gl::detail::BufferInterface<true>;
+
 		VertexBuffer() noexcept;
 		~VertexBuffer() noexcept = default;
 
 		void Assign(std::initializer_list<std::int32_t> list, buffer::BufferUsage usage = buffer::BufferUsage::StaticDraw) noexcept;
 		void Assign(std::initializer_list<std::uint32_t> list, buffer::BufferUsage usage = buffer::BufferUsage::StaticDraw) noexcept;
 		void Assign(std::initializer_list<float> list, buffer::BufferUsage usage = buffer::BufferUsage::StaticDraw) noexcept;
-		void Assign(const void* const& data, const size_t& size, buffer::BufferUsage usage = buffer::BufferUsage::StaticDraw) noexcept;
-		void SetLayout(const BufferLayout& layout) noexcept;
-		void SetLayout(BufferLayout&& layout) noexcept;
-
-		void Bind() const noexcept;
-		void Unbind() const noexcept;
-		void Use() const noexcept;
-
-		[[nodiscard]] buffer::BufferUsage GetUsage() const noexcept;
-		[[nodiscard]] size_t GetSize() const noexcept;
-		[[nodiscard]] BufferObject& GetHandle() noexcept;
-		[[nodiscard]] const BufferObject& GetHandle() const noexcept;
 
 		VertexBuffer(const VertexBuffer&) = delete;
 		VertexBuffer(VertexBuffer&&) noexcept = default;
 		VertexBuffer& operator=(const VertexBuffer&) = delete;
 		VertexBuffer& operator=(VertexBuffer&&) noexcept = default;
-
-	private:
-		BufferObject myHandle;
 	};
 }
--- OpenGL/src/VertexBuffer.cpp
+++ OpenGL/src/VertexBuffer.cpp
@@ -5,97 +5,26 @@ import :VertexBuffer;
 
 gl::VertexBuffer::VertexBuffer()
 noexcept
-	: myHandle(buffer::BufferType::Array)
+	: base(buffer::BufferType::Array)
 {}
 
 void
 gl::VertexBuffer::Assign(std::initializer_list<std::int32_t> list, buffer::BufferUsage usage)
 noexcept
 {
-	this->Assign(list.begin(), list.size(), usage);
+	base::SetData(list.begin(), list.size(), usage);
 }
 
 void
 gl::VertexBuffer::Assign(std::initializer_list<std::uint32_t> list, buffer::BufferUsage usage)
 noexcept
 {
-	this->Assign(list.begin(), list.size(), usage);
+	base::SetData(list.begin(), list.size(), usage);
 }
 
 void
 gl::VertexBuffer::Assign(std::initializer_list<float> list, buffer::BufferUsage usage)
 noexcept
 {
-	this->Assign(list.begin(), list.size(), usage);
-}
-
-void
-gl::VertexBuffer::Assign(const void* const& data, const size_t& size, buffer::BufferUsage usage)
-noexcept
-{
-	myHandle.SetData(data, size, usage);
-}
-
-void
-gl::VertexBuffer::SetLayout(const BufferLayout& layout)
-noexcept
-{
-	myHandle.SetLayout(layout);
-}
-
-void
-gl::VertexBuffer::SetLayout(BufferLayout&& layout)
-noexcept
-{
-	myHandle.SetLayout(std::move(layout));
-}
-
-void
-gl::VertexBuffer::Bind()
-const noexcept
-{
-	myHandle.Bind();
-}
-
-void
-gl::VertexBuffer::Unbind()
-const noexcept
-{
-	myHandle.Unbind();
-}
-
-void
-gl::VertexBuffer::Use()
-const noexcept
-{
-	myHandle.Use();
-}
-
-gl::buffer::BufferUsage
-gl::VertexBuffer::GetUsage()
-const noexcept
-{
-	return myHandle.GetUsage();
-}
-
-size_t
-gl::VertexBuffer::GetSize()
-const noexcept
-{
-	return myHandle.GetSize();
-
-}
-
-gl::BufferObject&
-gl::VertexBuffer::GetHandle()
-noexcept
-{
-	return myHandle;
-}
-
-const gl::BufferObject&
-gl::VertexBuffer::GetHandle()
-const noexcept
-{
-	return myHandle;
+	base::SetData(list.begin(), list.size(), usage);
 }
