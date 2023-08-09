856d5847ee232bbc29410f4d4595ce04ab27dfd0
Iconer <yoyofa2@hotmail.com>
2023-08-09 20:51:42 +0900

    인덱스 버퍼는 수정 가능한 레이아웃을 제공하는 구현체를 상속함

--- OpenGL/IndexBuffer.ixx
+++ OpenGL/IndexBuffer.ixx
@@ -6,35 +6,21 @@ import :BufferObject;
 
 export namespace gl
 {
-	class [[nodiscard]]
-		alignas(std::hardware_constructive_interference_size)
-			IndexBuffer
+	class [[nodiscard]] IndexBuffer 
+		: public gl::detail::BufferInterface<true>
 	{
 	public:
+		using base = gl::detail::BufferInterface<true>;
+
 		IndexBuffer() noexcept;
 		~IndexBuffer() noexcept = default;
 
 		void Assign(std::initializer_list<std::int32_t> list, buffer::BufferUsage usage = buffer::BufferUsage::StaticDraw) noexcept;
 		void Assign(std::initializer_list<std::uint32_t> list, buffer::BufferUsage usage = buffer::BufferUsage::StaticDraw) noexcept;
-		void Assign(const void* const& data, const size_t& size, buffer::BufferUsage usage = buffer::BufferUsage::StaticDraw) noexcept;
-		void SetLayout(const BufferLayout& layout) noexcept;
-		void SetLayout(BufferLayout&& layout) noexcept;
-
-		void Bind() const noexcept;
-		void Unbind() const noexcept;
-		void Use() const noexcept;
-
-		[[nodiscard]] buffer::BufferUsage GetUsage() const noexcept;
-		[[nodiscard]] size_t GetSize() const noexcept;
-		[[nodiscard]] BufferObject& GetHandle() noexcept;
-		[[nodiscard]] const BufferObject& GetHandle() const noexcept;
 
 		IndexBuffer(const IndexBuffer&) = delete;
 		IndexBuffer(IndexBuffer&&) noexcept = default;
 		IndexBuffer& operator=(const IndexBuffer&) = delete;
 		IndexBuffer& operator=(IndexBuffer&&) noexcept = default;
-
-	private:
-		BufferObject myHandle;
 	};
 }
--- OpenGL/src/IndexBuffer.cpp
+++ OpenGL/src/IndexBuffer.cpp
@@ -5,89 +5,19 @@ import :IndexBuffer;
 
 gl::IndexBuffer::IndexBuffer()
 noexcept
-	: myHandle(buffer::BufferType::ElementArray)
+	: base(buffer::BufferType::ElementArray)
 {}
 
 void
 gl::IndexBuffer::Assign(std::initializer_list<std::int32_t> list, buffer::BufferUsage usage)
 noexcept
 {
-	this->Assign(list.begin(), list.size(), usage);
+	base::SetData(list.begin(), list.size(), usage);
 }
 
 void
 gl::IndexBuffer::Assign(std::initializer_list<std::uint32_t> list, buffer::BufferUsage usage)
 noexcept
 {
-	this->Assign(list.begin(), list.size(), usage);
-}
-
-void
-gl::IndexBuffer::Assign(const void* const& data, const size_t& size, buffer::BufferUsage usage)
-noexcept
-{
-	myHandle.SetData(data, size, usage);
-}
-
-void
-gl::IndexBuffer::SetLayout(const BufferLayout& layout)
-noexcept
-{
-	myHandle.SetLayout(layout);
-}
-
-void
-gl::IndexBuffer::SetLayout(BufferLayout&& layout)
-noexcept
-{
-	myHandle.SetLayout(std::move(layout));
-}
-
-void
-gl::IndexBuffer::Bind()
-const noexcept
-{
-	myHandle.Bind();
-}
-
-void
-gl::IndexBuffer::Unbind()
-const noexcept
-{
-	myHandle.Unbind();
-}
-
-void
-gl::IndexBuffer::Use()
-const noexcept
-{
-	myHandle.Unbind();
-}
-
-gl::buffer::BufferUsage
-gl::IndexBuffer::GetUsage()
-const noexcept
-{
-	return myHandle.GetUsage();
-}
-
-size_t
-gl::IndexBuffer::GetSize()
-const noexcept
-{
-	return myHandle.GetSize();
-}
-
-gl::BufferObject&
-gl::IndexBuffer::GetHandle()
-noexcept
-{
-	return myHandle;
-}
-
-const gl::BufferObject&
-gl::IndexBuffer::GetHandle()
-const noexcept
-{
-	return myHandle;
+	base::SetData(list.begin(), list.size(), usage);
 }
