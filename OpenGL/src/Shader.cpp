9e1298f738807736f00975a9072ec1f21f0f08a2
Iconer <yoyofa2@hotmail.com>
2023-08-12 16:51:26 +0900

    쉐이더를 파일을 불러오는 동작까지 구현

--- OpenGL/src/Shader.cpp
+++ OpenGL/src/Shader.cpp
@@ -5,15 +5,130 @@ module;
 
 module Glib;
 import <type_traits>;
+import <string>;
+import <algorithm>;
+import <functional>;
+import Utility.IO.File;
 import :Shader;
+
+constinit static const GLchar* lastError = nullptr;
+inline constexpr std::string_view noError = "No Error";
 
 gl::Shader::Shader(shader::ShaderType sh_type)
 noexcept
 	: base()
 	, myType(sh_type)
-{
-}
+{}
 
 gl::Shader::~Shader()
 noexcept
-{}
+{
+	Destroy();
+}
+
+gl::shader::ErrorCode
+gl::Shader::LoadFrom(const std::filesystem::path& filepath)
+noexcept
+{
+	if (filepath.empty())
+	{
+		return shader::ErrorCode::EmptyFilePath;
+	}
+
+	if (!filepath.has_filename())
+	{
+		return shader::ErrorCode::InvalidFilePath;
+	}
+
+	util::io::File shfile{ filepath, util::io::file::OpenModes::Binary };
+
+	if (!shfile.IsOpen())
+	{
+		return shader::ErrorCode::FileDoesNotExists;
+	}
+
+	std::string_view contents = shfile.Contents();
+	if (!contents.empty())
+	{
+		return shader::ErrorCode::EmptyFile;
+	}
+
+	constexpr std::string_view ext_main{ "main" };
+
+	std::boyer_moore_horspool_searcher searcher{ ext_main.cbegin(), ext_main.cend() };
+	const auto it = std::search(contents.cbegin(), contents.cend(), searcher);
+
+	if (contents.cend() == it)
+	{
+		return shader::ErrorCode::NotValidShader;
+	}
+
+	return Complile(contents);
+}
+
+gl::shader::ErrorCode
+gl::Shader::Compile(std::string_view content)
+noexcept
+{
+	shader::ErrorCode result = shader::ErrorCode::Success;
+
+	return result;
+}
+
+void
+gl::Shader::Use(const unsigned int& program)
+noexcept
+{
+	::glUseShaderProgramEXT(static_cast<GLenum>(myType), GetID());
+}
+
+void
+gl::Shader::Destroy()
+noexcept
+{
+	if (IsLoaded() && IsCompiled())
+	{
+		::glDeleteShader(GetID());
+		SetID(0);
+	}
+}
+
+gl::shader::ShaderType
+gl::Shader::GetType()
+const noexcept
+{
+	return myType;
+}
+
+bool
+gl::Shader::IsLoaded()
+const noexcept
+{
+	return IsCompiled() && myType != gl::shader::ShaderType::None;
+}
+
+bool
+gl::Shader::IsUnloaded()
+const noexcept
+{
+	return !IsCompiled() || myType == gl::shader::ShaderType::None;
+}
+
+bool
+gl::Shader::IsCompiled()
+const noexcept
+{
+	return isCompiled;
+}
+
+std::string_view
+gl::Shader::GetLastError()
+noexcept
+{
+	if (nullptr == ::lastError)
+	{
+		return ::noError;
+	}
+
+	return std::string_view{ ::lastError };
+}
